510c60eba18896a41784e5e09ae0666c346c8989
syedmdfarhaneazam <syedmdfarhaneazam@gmail.com>
2024-11-01 15:18:48 +0000

    Contacts
    addded contact details and the template is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import Card from "./components/Card";
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
-
 function App() {
     return (
         <div className="app">
             <Header />
             <Quiz />
-            <Card />
         </div>
     )
 }
-
 export default App;
